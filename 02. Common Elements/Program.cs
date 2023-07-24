ec337e58620550a919c43220147f91fbec2dce78
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-07-24 04:34:48 +0300

    Add files via upload

---
+++ 02. Common Elements/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] firstArray = Console.ReadLine().Split();
+string[] secondArray = Console.ReadLine().Split();
+
+for (int i = 0; i < secondArray.Length; i++)
+{
+    for (int j = 0; j < firstArray.Length; j++)
+    {
+        if (secondArray[i] == firstArray[j])
+        {
+            Console.Write(secondArray[i] + " ");
+        }
+    }
+}
