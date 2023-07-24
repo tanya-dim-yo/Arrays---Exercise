83698107fe8bcfa1f237acca10913f7e53ecd7cf
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-07-24 04:33:17 +0300

    Add files via upload

---
+++ 01. Train/Program.cs
@@ -0,0 +1,17 @@
+﻿int wagons = int.Parse(Console.ReadLine());
+int[] train = new int[wagons];
+int totalPassengersNumber = 0;
+
+for (int i = 0; i < train.Length; i++)
+{
+    train[i] = int.Parse(Console.ReadLine());
+    totalPassengersNumber += train[i];
+}
+
+for (int i = 0; i < train.Length; i++)
+{
+    Console.Write($"{train[i]} ");
+}
+
+Console.WriteLine();
+Console.WriteLine(totalPassengersNumber);
