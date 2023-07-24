9f2b160d8b53ff8a4504ef38c1b74257168ad981
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-07-24 04:35:26 +0300

    Add files via upload

---
+++ 03. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,31 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] firstArray = new int[n];
+int[] secondArray = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+    if (i % 2 == 0)
+    {
+        firstArray[i] = numbers[0];
+        secondArray[i] = numbers[1];
+    }
+    else
+    {
+        firstArray[i] = numbers[1];
+        secondArray[i] = numbers[0];
+    }
+}
+
+for (int i = 0; i < firstArray.Length; i++)
+{
+    Console.Write(firstArray[i] + " ");
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < secondArray.Length; i++)
+{
+    Console.Write(secondArray[i] + " ");
+}
