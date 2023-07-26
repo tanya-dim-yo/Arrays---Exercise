7fa2ae813333e9fdc1c322999d4b21b428b72eab
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-07-26 04:35:44 +0300

    Add files via upload

---
+++ 04. Array Rotation/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int rotationNumber = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= rotationNumber; i++)
+{
+    int firstElement = numbers[0];
+
+    for (int j = 0; j < numbers.Length - 1; j++)
+    {
+        numbers[j] = numbers[j + 1];
+    }
+
+    numbers[numbers.Length - 1] = firstElement;
+}
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write(numbers[i] + " ");
+}
