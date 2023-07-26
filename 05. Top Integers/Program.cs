c75fd85dbb719c6658daaeeabe1a5cde98f8a4fe
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-07-26 04:36:24 +0300

    Add files via upload

---
+++ 05. Top Integers/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    bool isBigger = true;
+
+    for (int j = i + 1; j < numbers.Length; j++)
+    {
+        if (numbers[i] <= numbers[j])
+        {
+            isBigger = false;
+        }
+    }
+
+    if (isBigger)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+}
