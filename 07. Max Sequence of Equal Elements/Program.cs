be79261800a631a99482d606c96392039c4b43d7
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-07-26 04:37:45 +0300

    Add files via upload

---
+++ 07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int longestSequenceStartIndex = 0;
+int longestSequenceLength = 0;
+
+for (int i = 1; i < numbers.Length; i++)
+{
+    int currentSequenceStartIndex = i - 1;
+    int currentSequenceLength = 1;
+
+    while (i < numbers.Length && numbers[i] == numbers[i - 1])
+    {
+        currentSequenceLength++;
+        i++;
+    }
+
+    if (currentSequenceLength > longestSequenceLength)
+    {
+        longestSequenceLength = currentSequenceLength;
+        longestSequenceStartIndex = currentSequenceStartIndex;
+    }
+}
+
+for (int i = longestSequenceStartIndex; i < longestSequenceStartIndex + longestSequenceLength; i++)
+{
+    Console.Write(numbers[i] + " ");
+}
