fd946ae7654834a0595fca613706846650b8d5eb
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-07-28 04:05:50 +0300

    Add files via upload

---
+++ 08. Magic Sum/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    for (int j = i + 1; j < numbers.Length; j++)
+    {
+        if (numbers[i] + numbers[j] == n)
+        {
+            Console.Write($"{numbers[i]} {numbers[j]}");
+            Console.WriteLine();
+        }
+    }
+}
